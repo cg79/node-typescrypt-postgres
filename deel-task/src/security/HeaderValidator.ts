a7323ee5ff42dc1ccff06b8e992b890fb38c0b78
Claudiu Gombos <claudiu.gombos@thinslices.com>
2024-03-06 09:40:26 +0200

    tsconfig

--- deel-task/src/security/HeaderValidator.ts
+++ deel-task/src/security/HeaderValidator.ts
@@ -1,27 +1,22 @@
-import { Request, Response, NextFunction } from "express";
-import { SECURITY } from "./constants";
-import { APIError } from "../handlers/apiError";
+import { Request, Response, NextFunction } from 'express';
+import { SECURITY } from './constants';
+import { APIError } from '../handlers/apiError';
 
 export function validateHeader(header: string | undefined) {
   if (!header) {
-    throw new APIError("Invalid header", 401);
+    throw new APIError('Invalid header', 401);
   }
 }
 
-export const headerValidator = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const headerValidator = async (req: Request, res: Response, next: NextFunction) => {
   // console.log(JSON.stringify(req.headers));
-
   try {
     debugger;
-    console.log("profile_id", JSON.stringify(req.query?.profile_id));
-    validateHeader((req.headers[SECURITY.HEADER] || "") as string);
+    console.log('profile_id', JSON.stringify(req.query?.profile_id));
+    validateHeader((req.headers[SECURITY.HEADER] || '') as string);
     next();
   } catch (error: any) {
-    console.log("header error");
+    console.log('header error');
     next(error);
   }
 };
