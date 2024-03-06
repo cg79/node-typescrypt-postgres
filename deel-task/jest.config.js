d0a96179e7a621ebedcf6b0a4365aa4775971b24
Claudiu Gombos <claudiu.gombos@thinslices.com>
2024-03-06 10:42:08 +0200

    coverage and test

---
+++ deel-task/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  verbose: true,
+  testPathIgnorePatterns: ['node_modules'],
+  roots: ['<rootDir>/src'],
+};
